e5012d267ee3892c5aab4556b35cb4447302672c
Darren St. Amour <darren@nooperation.org>
2010-01-18 02:42:35 -0500

    Added a debug class.
    http://battlecode.mit.edu/2010/docs/specs.html#toc60
    
    The game supports debug code that doesn't count against the bot's BCs.
    After a bit of testing, it seems to be actually cheaper to have the
    methods in their own class instead of in the individual bot files.

---
+++ dpns_bot/Debugger.java
@@ -0,0 +1,17 @@
+package dpns_bot;
+
+import battlecode.common.Clock;
+
+public class Debugger {
+    public static void debug_print(String str) {
+        System.out.println(str);
+    }
+
+    public static void debug_print_bc_used() {
+        System.out.println("BC Used: " + Clock.getBytecodeNum());
+    }
+    
+    public static void debug_print_total_bc_used() {
+        System.out.println("Total BC Used: " + Clock.getBytecodeNum());
+    }
+}
