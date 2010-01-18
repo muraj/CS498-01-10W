9c7edcb764b0fdb445874448475d1b2b05da019d
Darren St. Amour <darren@nooperation.org>
2010-01-17 22:41:43 -0500

    Determine the type of bot only once.

--- dpns_bot/RobotPlayer.java
+++ dpns_bot/RobotPlayer.java
@@ -1,38 +1,80 @@
 package dpns_bot;
 
-import battlecode.common.*;
-import static battlecode.common.GameConstants.*;
+import battlecode.common.Clock;
+import battlecode.common.RobotController;
+import battlecode.common.RobotType;
 
 public class RobotPlayer implements Runnable {
-
-    private final RobotController myRC;
+    private final RobotController rc;
 
     public RobotPlayer(RobotController rc) {
-        myRC = rc;
-    }
+        this.rc = rc;
+   }
 
     public void run() {
-        //System.out.println("STARTING");
-        while (true) {
-            try {
-                /*** beginning of main loop ***/
-                while (myRC.isMovementActive()) {
-                    myRC.yield();
-                }
+        Bot b = null;
 
-                if (myRC.canMove(myRC.getDirection())) {
-                    System.out.println("about to move");
-                    myRC.moveForward();
-                } else {
-                    myRC.setDirection(myRC.getDirection().rotateRight());
-                }
-                myRC.yield();
+        System.out.println("RB->RUN()");
 
-                /*** end of main loop ***/
-            } catch (Exception e) {
-                System.out.println("caught exception:");
-                e.printStackTrace();
-            }
+        switch(rc.getRobotType()) {
+            case ARCHON:
+                b = new ArchonBot(rc);
+            break;
+
+            case AURA:
+                System.out.println("BasdfadsYECODEZ: " + Clock.getBytecodeNum());
+            break;
+
+            case CHAINER:
+                System.out.println("BYECODasasEZ: " + Clock.getBytecodeNum());
+            break;
+
+            case COMM:
+                System.out.println("BYECODadsfEZ: " + Clock.getBytecodeNum());
+            break;
+
+            case SOLDIER:
+                System.out.println("BYECODadsfEZ: " + Clock.getBytecodeNum());
+            break;
+
+            case TELEPORTER:
+                System.out.println("BYECODEadsfaZ: " + Clock.getBytecodeNum());
+            break;
+
+            case TURRET:
+                System.out.println("BYECODEasdfZ: " + Clock.getBytecodeNum());
+            break;
+
+            case WOUT:
+                System.out.println("BalkjkklkjYECODEZ: " + Clock.getBytecodeNum());
+            break;
+
+            default:
+                System.out.println("Robot Type not supported yet.");
+            break;
+        }
+
+        b.run();
+     }
+}
+
+class Bot {
+    public RobotController rc;
+
+    public void run() {
+        System.out.println("Nothing to see here");
+    }
+}
+
+class ArchonBot extends Bot {
+    public ArchonBot(RobotController rc) {
+        this.rc = rc;
+    }
+    
+    public void run() {
+        while(true) {
+            System.out.println("Bytecodes used: " + Clock.getBytecodeNum());
+            rc.yield();
         }
     }
 }
