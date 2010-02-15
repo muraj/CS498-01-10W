c87843375b4167d613a156f18cce71cbdea95450
Darren St. Amour <darren@nooperation.org>
2010-02-15 02:43:22 -0500

    Added currentLocation to the Bot class.
    Figured each bot should have an easier way of keeping up with its
    current location. This should be updated on every move.

--- team276/Bot.java
+++ team276/Bot.java
@@ -8,10 +8,13 @@ public abstract class Bot {
     protected final Team team;
     protected final int id;
     protected int bcCounterStart;
+    protected MapLocation currentLocation;
+
     public Bot(RobotController rc, Team t) {
         this.rc = rc;
         this.id = rc.getRobot().getID();
         this.team = t;
+        this.currentLocation = rc.getLocation();
     }
 
     public abstract void AI() throws Exception;
