4c656989d7a6f4ec4444306b541dd485304eb2e5
Darren St. Amour <darren@nooperation.org>
2010-02-16 01:27:07 -0500

    Cleaned up some fields in the Generic Bot class.
    Added a leader, following field, and current location field to the
    generic bot. Also removed the currently unused origin field.
    
    The bots are going to have to know if they're a leader or who they're
    following if they're not a leader.

--- team276/Bot.java
+++ team276/Bot.java
@@ -7,12 +7,16 @@ public abstract class Bot {
     protected final Team team;
     protected final int id;
     protected int bcCounterStart;
-    protected MapLocation origin;
+    protected MapLocation currentLoc;
+    protected boolean isLeader;
+    protected int following;
 
     public Bot(RobotController rc, Team t) {
         this.rc = rc;
         this.id = rc.getRobot().getID();
         this.team = t;
+        this.isLeader = false;
+        this.currentLoc = rc.getLocation();
     }
 
     public abstract void AI() throws Exception;
