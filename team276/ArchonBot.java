4564bb153b87d9ef2c56623ae46d2b4bea232023
Darren St. Amour <darren@nooperation.org>
2010-02-16 03:57:08 -0500

    WIP: Playing around with my own breakout.
    The lowest number Archon spits out a message informing the others of his
    leadership role.

--- team276/ArchonBot.java
+++ team276/ArchonBot.java
@@ -8,43 +8,68 @@ public class ArchonBot extends Bot {
     }
 
     private void breakout() throws Exception {
-        MapLocation myloc = rc.getLocation();
-        int dx = 0,
-            dy = 0;
-        MapLocation[] alliedArchonLocs = rc.senseAlliedArchons();
-        Direction dTmp;
+        Debugger.debug_print("breakout(): Enter");
+        Debugger.debug_print_bc_used();
+
+        /* Sense the friendlies and figure out who's leading */
+        MapLocation[] alliedArchons = rc.senseAlliedArchons();
+        boolean pLeader = true;
         
-        for(MapLocation i: alliedArchonLocs) {
-            Direction tmp = myloc.directionTo(i);
-            dx -= tmp.dx;
-            dy -= tmp.dy;
+        for(MapLocation i: alliedArchons) {
+            if(id > rc.senseAirRobotAtLocation(i).getID()) {
+                pLeader = false;
+                break;
+            }
         }
 
-        dTmp = Util.coordToDirection(dx, dy);
-        rc.setDirection(dTmp);
-
-        if(dTmp == Direction.NORTH_EAST) {
-            origin = myloc;
-
+        /* We're the leader. Queue up a message with our ID in it and then attempt
+         * to see if we're close to an edge. */
+        if(pLeader) {
+            Debugger.debug_print("I'm the leader.");
+            isLeader = true;
             Message msg = new Message();
-            msg.locations = new MapLocation[] { origin };
+            msg.ints = new int[] { id };
             rc.broadcast(msg);
+
+            /* Only scan the far out parts-- if we're near an edge we'll know. */
+            int myX = currentLoc.getX();
+            int myY = currentLoc.getY();
+
+            /* Check N */
+            if(rc.senseTerrainTile(new MapLocation(myX + 0, myY - 6)).getType() == TerrainTile.TerrainType.OFF_MAP)
+                Debugger.debug_print("Close to North boundry!");
+
+            /* Check S */
+            else if(rc.senseTerrainTile(new MapLocation(myX + 0, myY + 6)).getType() == TerrainTile.TerrainType.OFF_MAP)
+                Debugger.debug_print("Close to South boundry!");
+
+            /* Check E */
+            if(rc.senseTerrainTile(new MapLocation(myX + 6, myY + 0)).getType() == TerrainTile.TerrainType.OFF_MAP)
+                Debugger.debug_print("Close to East boundry!");
+
+            /* Check W */
+            else if(rc.senseTerrainTile(new MapLocation(myX + -6, myY + 0)).getType() == TerrainTile.TerrainType.OFF_MAP)
+                Debugger.debug_print("Close to West boundry!");
         }
+
+        /* Otherwise, get the incoming message */
+        else {
+            Message msg = rc.getNextMessage();
+
+            /* XXX Do some sort of message validation here XXX */
+
+            following = msg.ints[0];
+            Debugger.debug_print("I'm following bot id: " + following);
+        }
+
+        Debugger.debug_print("breakout(): Exit");
+        Debugger.debug_print_bc_used();
     }
 
     public void AI() throws Exception{
-        Debugger.debug_set_counter(this);
         breakout();
-        Debugger.debug_print_counter(this);
-        rc.yield();
-        rc.moveForward();
+
         while(true){
-            Debugger.debug_set_counter(this);
-            Debugger.debug_print("I'm an Archon!");
-            Debugger.debug_print_energon(this.rc);
-            Debugger.debug_print_counter(this);
-            //Handle communication here.
-            Debugger.debug_print_total_bc_used();
             rc.yield();
         }
     }
