8b93def26f498e131e98a171e6ddb8a9c4b3537e
Darren St. Amour <darren@nooperation.org>
2010-02-22 02:53:19 -0500

    All bots now track their current location.

--- team276/Bot.java
+++ team276/Bot.java
@@ -8,12 +8,14 @@ public abstract class Bot {
     protected final int id;
     protected int bcCounterStart;
     protected MapLocation currentLocation;
+    protected Direction currentDirection;
 
     public Bot(RobotController rc, Team t) {
         this.rc = rc;
         this.id = rc.getRobot().getID();
         this.team = t;
         this.currentLocation = rc.getLocation();
+        this.currentDirection = rc.getDirection();
     }
 
     public abstract void AI() throws Exception;
