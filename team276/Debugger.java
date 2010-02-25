5487bbd7442eeea2a19dd76fb76210139551d0cd
Cory Perry <killogge@gmail.com>
2010-02-25 03:48:45 -0500

    Made some methods final, equivalent to C's inline

--- team276/Debugger.java
+++ team276/Debugger.java
@@ -3,27 +3,27 @@ package team276;
 import battlecode.common.*;
 
 public class Debugger {
-    public static void debugPrint(String str) {
+    public static final void debugPrint(String str) {
         System.out.println(str);
     }
 
-    public static void debugPrintBCUsed() {
+    public static final void debugPrintBCUsed() {
         System.out.println("BC Used: " + Clock.getBytecodeNum());
     }
 
-    public static void debugPrintTotalBCUsed() {
+    public static final void debugPrintTotalBCUsed() {
         System.out.println("Total BC Used: " + Clock.getBytecodeNum());
     }
 
-    public static void debugPrintEnergon(RobotController rc) {
+    public static final void debugPrintEnergon(RobotController rc) {
         System.out.println("Energon: " + rc.getEnergonLevel());
     }
 
-    public static void debugSetCounter(Bot b) {
+    public static final void debugSetCounter(Bot b) {
         b.bcCounterStart = Clock.getBytecodeNum();
     }
 
-    public static void debugPrintCounter(Bot b) {
+    public static final void debugPrintCounter(Bot b) {
         System.out.println("Total executed bytecodes:"+ (Clock.getBytecodeNum() - b.bcCounterStart));
     }
 }
