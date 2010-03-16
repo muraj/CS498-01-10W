7c45eba4f95abff20ae19c0dce006e637f3f7c1e
Darren St. Amour <darren@nooperation.org>
2010-03-16 19:37:54 -0400

    Removed Archon breakout.
    This was just some small test code that blows up whenever the Archons
    can't make their first moves due to lack of error checking.

--- team276/ArchonBot.java
+++ team276/ArchonBot.java
@@ -7,36 +7,7 @@ public class ArchonBot extends Bot {
         super(rc,t);
     }
 
-    private void breakout() throws Exception {
-        int dx = 0;
-        int dy = 0;
-
-        MapLocation[] alliedArchonLocs = rc.senseAlliedArchons();
-        Direction dTmp;
-
-        for (MapLocation i: alliedArchonLocs) {
-            Direction tmp = currentLocation.directionTo(i);
-            dx -= tmp.dx;
-            dy -= tmp.dy;
-        }
-
-        dTmp = Util.coordToDirection(dx, dy);
-        rc.setDirection(dTmp);
-
-        if (dTmp == Direction.NORTH_EAST) {
-            Message msg = new Message();
-            msg.locations = new MapLocation[] { currentLocation };
-            rc.broadcast(msg);
-        }
-    }
-
     public void AI() throws Exception {
-        Debugger.debug_setCounter(this);
-        breakout();
-        Debugger.debug_printCounter(this);
-        rc.yield();
-        rc.moveForward();
-
         while (true) {
             Debugger.debug_setCounter(this);
             Debugger.debug_print("I'm an Archon!");
